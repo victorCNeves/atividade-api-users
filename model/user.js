8ee446b812737e9de6d4db9048fa0bf621788e2e
victorCNeves <105159565+victorCNeves@users.noreply.github.com>
2026-05-28 17:09:41 -0300

    style: formatação dos arquivos com prettier

--- model/user.js
+++ model/user.js
@@ -1,24 +1,24 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "O nome é obrigatório."],
-        trim: true
-    },
-    email: {
-        type: String,
-        required: [true, "O e-mail é obrigatório."],
-        unique: true,
-        trim: true,
-        lowercase: true,
-        match: [/^[\w-\.\+]+@([\w-]+\.)+[\w-]{2,4}$/, "Insira um e-mail válido."]
-    },
-    password: {
-        type: String,
-        required: [true, "A senha é obrigatória."],
-        minLength: [8, "A senha deve conter no mínimo 8 caracteres."]
-    },
+  name: {
+    type: String,
+    required: [true, "O nome é obrigatório."],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "O e-mail é obrigatório."],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/^[\w-\.\+]+@([\w-]+\.)+[\w-]{2,4}$/, "Insira um e-mail válido."],
+  },
+  password: {
+    type: String,
+    required: [true, "A senha é obrigatória."],
+    minLength: [8, "A senha deve conter no mínimo 8 caracteres."],
+  },
 });
 
 export const User = mongoose.model("User", userSchema);
